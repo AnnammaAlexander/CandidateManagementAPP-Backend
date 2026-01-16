e74dcd825c936185976ab56dc8fe12b2d80f3f0c
Annamma K.A <117367068+AnnammaAlexander@users.noreply.github.com>
2026-01-16 10:26:58 +0530

    Clean up comments in candidates.js
    Removed comment annotations for the getMe function.

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -1,9 +1,8 @@
 import asyncHandler from '../middlewares/asyncHandler.js';
 import sendResponse from '../utils/response.js';
 
-// @desc    Get current logged in user profile
-// @route   GET /candidates/me
-// @access  Private
+// Get current logged in user profile
+
 export const getMe = asyncHandler(async (req, res, next) => {
     sendResponse(res, 200, true, 'User profile fetched successfully', req.user);
 });
