75e6387a51ad9b86039377d1c1057ef3d3647267
Annamma K.A <117367068+AnnammaAlexander@users.noreply.github.com>
2026-01-16 10:27:14 +0530

    Remove unnecessary empty line in getMe function

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -2,7 +2,6 @@ import asyncHandler from '../middlewares/asyncHandler.js';
 import sendResponse from '../utils/response.js';
 
 // Get current logged in user profile
-
 export const getMe = asyncHandler(async (req, res, next) => {
     sendResponse(res, 200, true, 'User profile fetched successfully', req.user);
 });
